04ba67484f3e878fc89b8c701487b57c8f1bbde0
viejo bucanero <lopezrunco@gmail.com>
2024-08-31 15:14:42 +0530

    feat: Add test endpoint.

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -5,6 +5,9 @@ const routes = (app) => {
     app.get("/", (req, res) => {
         res.send("TypeScript API running.");
     });
+    app.get("/ping", (_req, res) => {
+        return res.send("pong");
+    });
 };
 exports.routes = routes;
 //# sourceMappingURL=index.js.map
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,4 +7,8 @@ export const routes = (app: Express) => {
   app.get("/", (req: RequestType, res: ResponseType) => {
     res.send("TypeScript API running.");
   });
+  
+  app.get("/ping", (_req: Request, res: Response) => {
+    return res.send("pong");
+  });
 };
