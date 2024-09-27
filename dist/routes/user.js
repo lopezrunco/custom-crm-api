c16c82fcd8f5125949553207da823d813a0efdcf
viejo bucanero <lopezrunco@gmail.com>
2024-09-27 11:55:30 +0530

    refactor: Rename user controllers in user routes for better consistency.

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const register = require("../controllers/user/register");
-const login = require("../controllers/user/login");
-const getAll = require("../controllers/user/getAll");
-const getById = require("../controllers/user/getById");
+const userRegister = require("../controllers/user/register");
+const userLogin = require("../controllers/user/login");
+const getAllUsers = require("../controllers/user/getAll");
+const getUserById = require("../controllers/user/getById");
 const userRouter = (0, express_1.Router)();
-userRouter.post("/login", login);
-userRouter.post("/register", register);
-userRouter.get("/", getAll);
-userRouter.get("/:id", getById);
+userRouter.post("/login", userLogin);
+userRouter.post("/register", userRegister);
+userRouter.get("/", getAllUsers);
+userRouter.get("/:id", getUserById);
 exports.default = userRouter;
 //# sourceMappingURL=user.js.map
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 
-const register = require("../controllers/user/register");
-const login = require("../controllers/user/login");
-const getAll = require("../controllers/user/getAll");
-const getById = require("../controllers/user/getById");
+const userRegister = require("../controllers/user/register");
+const userLogin = require("../controllers/user/login");
+const getAllUsers = require("../controllers/user/getAll");
+const getUserById = require("../controllers/user/getById");
 
 const userRouter = Router();
 
-userRouter.post("/login", login);
-userRouter.post("/register", register);
-userRouter.get("/", getAll); 
-userRouter.get("/:id", getById); 
+userRouter.post("/login", userLogin);
+userRouter.post("/register", userRegister);
+userRouter.get("/", getAllUsers); 
+userRouter.get("/:id", getUserById); 
 
 export default userRouter;
